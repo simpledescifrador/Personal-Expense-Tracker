d1baa087da5af4075f3e298506e6843214040376
Edison Villarta <edtvillarta@outlook.com>
2020-05-12 13:55:16 +0800

    Extract transaction item widget

---
+++ lib/widgets/transaction_item.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
+
+class TransactionItem extends StatelessWidget {
+  const TransactionItem({
+    Key key,
+    @required Transaction transaction,
+    @required Function deleteTransaction,
+  }) : _userTransaction = transaction, _deleteTransaction = deleteTransaction, super(key: key);
+
+  final Function _deleteTransaction;
+  final Transaction _userTransaction;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+      child: ListTile(
+        leading: CircleAvatar(
+          radius: 30,
+          child: Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: FittedBox(
+              child: Text(
+                '\$${_userTransaction.amount.toStringAsFixed(2)}',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+          ),
+        ),
+        trailing: MediaQuery.of(context).size.width > 460
+            ? FlatButton.icon(
+                textColor: Theme.of(context).errorColor,
+                onPressed: () =>
+                    _deleteTransaction(_userTransaction.id),
+                icon: Icon(Icons.delete),
+                label: Text('Delete'),
+              )
+            : IconButton(
+                icon: Icon(Icons.delete),
+                color: Theme.of(context).errorColor,
+                onPressed: () =>
+                    _deleteTransaction(_userTransaction.id),
+              ),
+        title: Text(
+          _userTransaction.title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        subtitle: Text(
+          DateFormat.yMMMEd().format(_userTransaction.date),
+          style: TextStyle(
+            color: Colors.grey,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,12 +1,13 @@
-import 'package:expense_tracker/models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
+import './transaction_item.dart';
 
 class TransactionList extends StatelessWidget {
-  final List<Transaction> _userTransactions;
-  final Function _deleteTransaction;
-
   TransactionList(this._userTransactions, this._deleteTransaction);
+
+  final Function _deleteTransaction;
+  final List<Transaction> _userTransactions;
 
   @override
   Widget build(BuildContext context) {
@@ -34,51 +35,9 @@ class TransactionList extends StatelessWidget {
         : ListView.builder(
             itemCount: _userTransactions.length,
             itemBuilder: (ctx, index) {
-              return Card(
-                elevation: 5,
-                margin: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    radius: 30,
-                    child: Padding(
-                      padding: const EdgeInsets.all(6.0),
-                      child: FittedBox(
-                        child: Text(
-                          '\$${_userTransactions[index].amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20.0,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  trailing: MediaQuery.of(context).size.width > 460
-                      ? FlatButton.icon(
-                          textColor: Theme.of(context).errorColor,
-                          onPressed: () =>
-                              _deleteTransaction(_userTransactions[index].id),
-                          icon: Icon(Icons.delete),
-                          label: Text('Delete'),
-                        )
-                      : IconButton(
-                          icon: Icon(Icons.delete),
-                          color: Theme.of(context).errorColor,
-                          onPressed: () =>
-                              _deleteTransaction(_userTransactions[index].id),
-                        ),
-                  title: Text(
-                    _userTransactions[index].title,
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  subtitle: Text(
-                    DateFormat.yMMMEd().format(_userTransactions[index].date),
-                    style: TextStyle(
-                      color: Colors.grey,
-                    ),
-                  ),
-                ),
-              );
+              return TransactionItem(
+                  transaction: _userTransactions[index],
+                  deleteTransaction: _deleteTransaction);
             },
           );
   }
